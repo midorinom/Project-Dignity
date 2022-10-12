718e86ef279468356faf536c51155adfbf635f27
onthewaysia <atsqw001@gmail.com>
2022-10-12 23:35:50 +0800

    project init

---
+++ job-seeker-profile-dummy.js
@@ -0,0 +1,25 @@
+[
+  {
+    about: {
+      name: "Tan Li Choon",
+      aspiration: "Librarian",
+      brand:
+        "I am a literature enthusiast whom hopes to pursue his passion as a career",
+      email: "tanlichoon@gmail.com",
+      mobile: "+65 9273 2719",
+    },
+    skills: [
+      {
+        skill: "Digital Marketing",
+        cert: "Google Certificate for Digital Marketing",
+        issuer: "Google",
+        issueMonth: "January",
+        issueYear: "2020",
+      },
+    ],
+    abilityDifferences: {},
+    experience: {},
+    education: {},
+    fieldTypes: {},
+  },
+];
