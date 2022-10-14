bd45fc944f85e1b93eb17502d0f1922b5f4c21ac
haofang <haofang97@gmail.com>
2022-10-14 22:14:07 +0800

    added new "pages" directory

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import EmployerLanding from "./components/EmployerLanding";
-import JobSeekerLanding from "./components/JobSeekerLanding";
-import UniversalLanding from "./components/UniversalLanding";
 import Footer from "./components/Footer";
-import JobListings from "./components/JobListings";
-import Login from "./components/Login";
-import JobSeekerProfile from "./components/JobSeekerProfile";
-import EmployerProfile from "./components/EmployerProfile";
-import JobSeekerProfileForm from "./components/JobSeekerProfileForm";
-import JobUploadForm from "./components/JobUploadForm";
-import JobPost from "./components/JobPost";
+import EmployerLanding from "./pages/EmployerLanding";
+import JobSeekerLanding from "./pages/JobSeekerLanding";
+import UniversalLanding from "./pages/UniversalLanding";
+import JobListings from "./pages/JobListings";
+import Login from "./pages/Login";
+import JobSeekerProfile from "./pages/JobSeekerProfile";
+import EmployerProfile from "./pages/EmployerProfile";
+import JobSeekerProfileForm from "./pages/JobSeekerProfileForm";
+import JobUploadForm from "./pages/JobUploadForm";
+import JobPost from "./pages/JobPost";
 
 function App() {
   // Change this initial value to jobSeeker/employer if you need to access those landing/profile pages
