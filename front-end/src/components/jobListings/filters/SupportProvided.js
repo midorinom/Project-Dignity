90ac048a29b94c5a3de1daaa7edafc754a3bcd13
onthewaysia <atsqw001@gmail.com>
2022-10-16 00:21:40 +0800

    job listings filters added

---
+++ front-end/src/components/jobListings/filters/SupportProvided.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const SupportProvided = () => {
+  return (
+    <div className="mt-3 d-flex flex-column w-100 bg-light align-center">
+      <h5 className="mx-1 mb-1 mt-2 text-muted">Support Provided</h5>
+      <div className="btn btn-outline-secondary btn-sm m-1 d-flex justify-content-center">
+        <p className="d-flex justify-content-center m-0">Training Programmes</p>
+      </div>
+      <div className="btn btn-outline-secondary btn-sm m-1 d-flex justify-content-center">
+        <p className="d-flex justify-content-center m-0">Job Shadowing</p>
+      </div>
+      <div className="btn btn-outline-secondary btn-sm m-1 d-flex justify-content-center">
+        <p className="d-flex justify-content-center m-0">Trial Period</p>
+      </div>
+      <div className="btn btn-outline-secondary btn-sm m-1 d-flex justify-content-center">
+        <p className="d-flex justify-content-center m-0">Workplace Redesign</p>
+      </div>
+      <div className="btn btn-outline-secondary btn-sm m-1 d-flex justify-content-center">
+        <p className="d-flex justify-content-center m-0">Social Integration</p>
+      </div>
+      <div className="btn btn-outline-secondary btn-sm m-1 d-flex justify-content-center">
+        <p className="d-flex justify-content-center m-0">
+          Assistive Technology (AT)
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SupportProvided;
--- front-end/src/pages/JobListings.js
+++ front-end/src/pages/JobListings.js
@@ -1,7 +1,25 @@
 import React from "react";
+import AbilityDifference from "../components/jobListings/filters/AbilityDifference/AbilityDifference";
+import JobEnvironment from "../components/jobListings/filters/JobEnvironment";
+import JobInteractionType from "../components/jobListings/filters/JobInteractionType";
+import SupportProvided from "../components/jobListings/filters/SupportProvided";
 
 const JobListings = () => {
-  return <div className="job-listings centered">Job Listings</div>;
+  return (
+    <div className="job-listings">
+      {/* Search bar should be here */}
+      <h1 className="w-100 text-left">Dignity Career</h1>
+      <div className="results d-flex">
+        <div className="filters w-25 p-4">
+          <AbilityDifference />
+          <JobEnvironment />
+          <JobInteractionType />
+          <SupportProvided />
+        </div>
+        <div className="postings"></div>
+      </div>
+    </div>
+  );
 };
 
 export default JobListings;
