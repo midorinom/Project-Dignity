29763feb5c1a303cbec760f388199e8505b783b4
haofang <haofang97@gmail.com>
2022-10-15 14:14:34 +0800

    added NavBarRight

--- front-end/src/components/NavBar/NavBar.js
+++ front-end/src/components/NavBar/NavBar.js
@@ -2,17 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./navBar.module.css";
 import dignityCareersLogo from "./dignity-careers-logo.png";
+import profileIcon from "./profile-icon.png";
 
 const NavBar = () => {
   return (
-    <div className={`${styles.navBar} centered`}>
+    <div className={styles.navBar}>
       <div className={styles.dignityCareersLogoContainer}>
         <Link to="/" className={styles.linkDignityCareersLogo}>
           <img
             src={dignityCareersLogo}
             alt="Dignity Careers Logo"
             className={styles.dignityCareersLogo}
-          />
+          ></img>
         </Link>
         <p className={styles.dignityCareersName}>
           <Link to="/">
@@ -20,7 +21,25 @@ const NavBar = () => {
           </Link>
         </p>
       </div>
-      Nav Bar
+      <div className={styles.navBarRight}>
+        <div className={styles.accessibilityButton}>
+          Need help seeing this site better?
+        </div>
+        <p className={styles.jobListingsButton}>
+          <Link to="/job-listings">Job Listings </Link>
+        </p>
+        <div className={styles.profileIconContainer}>
+          <Link to="/profile">
+            <img
+              src={profileIcon}
+              alt="Profile Button"
+              className={styles.profileIcon}
+            />
+
+            <p>Profile</p>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
