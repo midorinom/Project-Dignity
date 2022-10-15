77d19b82384bab3b5f48d03b8db6e64f4d0255e0
midorinom <107839253+midorinom@users.noreply.github.com>
2022-10-15 23:21:03 +0800

    logic for displaying jobSeeker profile/profileForm

--- front-end/src/pages/jobSeekerProfile/JobSeekerProfile.js
+++ front-end/src/pages/jobSeekerProfile/JobSeekerProfile.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import dummyProfileData from "./job-seeker-profile-dummy";
 
 const JobSeekerProfile = () => {
-  return <div className="centered">Job Seeker Profile</div>;
+  const [profileIsCompleted, setProfileIsComplete] = useState(false);
+
+  function displayProfile() {
+    if (profileIsCompleted) {
+      return <div>Completed Job Seeker Profile</div>;
+    } else {
+      return (
+        <div className="centered" style={{ flexDirection: "column" }}>
+          <h1>You Don't Have a Profile Yet</h1>
+          <p>
+            Create one now, so your future employer can get to know you better!
+          </p>
+          <Link to="/profile-form">
+            <button>Create profile</button>
+          </Link>
+        </div>
+      );
+    }
+  }
+
+  return <>{displayProfile()}</>;
 };
 
 export default JobSeekerProfile;
