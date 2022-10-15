fc826b9dd8c8b09b84eaee90d82c070e10982b0f
midorinom <107839253+midorinom@users.noreply.github.com>
2022-10-16 01:36:51 +0800

    some css for job seeker completedProfile

--- front-end/src/pages/jobSeekerProfile/JobSeekerProfile.js
+++ front-end/src/pages/jobSeekerProfile/JobSeekerProfile.js
@@ -4,12 +4,14 @@ import styles from "./jobSeekerProfile.module.css";
 import dummyProfileData from "./job-seeker-profile-dummy";
 
 const JobSeekerProfile = () => {
-  const [profileIsCompleted, setProfileIsComplete] = useState(false);
+  // Change this profileIsCompleted initial value to false/true to access the NoProfile/CompletedProfile pages
+  const [profileIsCompleted, setProfileIsComplete] = useState(true);
+  const [profileData, setProfileData] = useState(dummyProfileData);
 
+  // Display either the NoProfile page or CompletedProfile page depending on whether profileIsCompleted
   function displayProfile() {
-    if (profileIsCompleted) {
-      return <div>Completed Job Seeker Profile</div>;
-    } else {
+    if (!profileIsCompleted) {
+      // NoProfile Page
       return (
         <div className={styles.noProfileJobSeeker}>
           <p className={styles.noProfileHeading}>
@@ -23,6 +25,29 @@ const JobSeekerProfile = () => {
               Create profile
             </button>
           </Link>
+        </div>
+      );
+    } else {
+      // CompletedProfile Page
+      return (
+        <div className={styles.completedProfileJobSeeker}>
+          <div className={styles.savePrintResumeContainer}>
+            <button className={styles.savePrintResumeButton}>
+              Save Resume as PDF
+            </button>
+            <button className={styles.savePrintResumeButton}>
+              Print Your Resume
+            </button>
+          </div>
+          <div className={styles.completedProfileJobSeekerBottom}>
+            <div className={styles.resume}>
+              <div className={styles.resumeBanner}>banner</div>
+              <div className={styles.resumeText}>resumeText</div>
+            </div>
+            <div className={styles.profileRecommendedJobs}>
+              Recommended Jobs
+            </div>
+          </div>
         </div>
       );
     }
