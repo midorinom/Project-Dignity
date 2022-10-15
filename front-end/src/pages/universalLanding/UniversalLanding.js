9188b3b6a03faa47ab1a81786bd2c756f26a4d8e
midorinom <107839253+midorinom@users.noreply.github.com>
2022-10-15 15:46:33 +0800

    renaming files

---
+++ front-end/src/pages/universalLanding/UniversalLanding.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./universalLanding.module.css";
+import banner from "./banner.png";
+import jobSeekerImage from "./jobSeekerImage.png";
+import employerImage from "./employerImage.png";
+
+const UniversalHome = () => {
+  return <div className={styles.universalLanding}>Universal Home</div>;
+};
+
+export default UniversalHome;
