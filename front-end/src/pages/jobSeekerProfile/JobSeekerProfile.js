39380bced101d4b72d424a7ecdf613fe83fc837b
midorinom <107839253+midorinom@users.noreply.github.com>
2022-10-15 23:37:03 +0800

    noProfileJobSeeker Page done

--- front-end/src/pages/jobSeekerProfile/JobSeekerProfile.js
+++ front-end/src/pages/jobSeekerProfile/JobSeekerProfile.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import styles from "./jobSeekerProfile.module.css";
 import dummyProfileData from "./job-seeker-profile-dummy";
 
 const JobSeekerProfile = () => {
@@ -10,13 +11,17 @@ const JobSeekerProfile = () => {
       return <div>Completed Job Seeker Profile</div>;
     } else {
       return (
-        <div className="centered" style={{ flexDirection: "column" }}>
-          <h1>You Don't Have a Profile Yet</h1>
-          <p>
+        <div className={styles.noProfileJobSeeker}>
+          <p className={styles.noProfileHeading}>
+            You Don't Have a Profile Yet
+          </p>
+          <p className={styles.noProfileText}>
             Create one now, so your future employer can get to know you better!
           </p>
           <Link to="/profile-form">
-            <button>Create profile</button>
+            <button className={styles.createProfileButton}>
+              Create profile
+            </button>
           </Link>
         </div>
       );
