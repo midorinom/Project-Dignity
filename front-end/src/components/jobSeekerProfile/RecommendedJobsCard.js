623a9b5926ab3c42bd6248d96340551cf43d7cfd
midorinom <107839253+midorinom@users.noreply.github.com>
2022-10-16 22:33:58 +0800

    jobSeekerProfile recommendedJobs section done

---
+++ front-end/src/components/jobSeekerProfile/RecommendedJobsCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./recommendedJobsCard.module.css";
+import saveIcon from "./saveIcon.png";
+
+const RecommendedJobsCard = (props) => {
+  return (
+    <div className={styles.recommendedJobsCard}>
+      <div className={styles.saveIconContainer}>
+        <img src={saveIcon} alt="Save Button" className={styles.saveIcon} />
+        <p className={styles.saveText}>Save</p>
+      </div>
+      <div className={styles.recommendedJobsCardText}>
+        <p>{props.jobData.jobTitle}</p>
+        <p>{props.jobData.employer}</p>
+        <p>
+          {props.jobData.employmentType} / {props.jobData.location}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default RecommendedJobsCard;
---
+++ front-end/src/pages/jobSeekerProfile/recommendedJobs-dummy.js
@@ -0,0 +1,22 @@
+const dummyRecommendedJobsData = [
+  {
+    jobTitle: "Assistant Pastry Chef",
+    employer: "VOXY",
+    employmentType: "Full-Time",
+    location: "Bugis",
+  },
+  {
+    jobTitle: "Junior Pastry Chef",
+    employer: "Angelina",
+    employmentType: "Full-Time",
+    location: "Boon Lay",
+  },
+  {
+    jobTitle: "Baker",
+    employer: "Breadtalk",
+    employmentType: "Full-Time",
+    location: "Bukit Batok",
+  },
+];
+
+export default dummyRecommendedJobsData;
