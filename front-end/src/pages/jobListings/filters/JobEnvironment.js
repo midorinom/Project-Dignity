8ad70c8ae7f3feeafa5c6e30ff05570b8fd2c63c
midorinom <107839253+midorinom@users.noreply.github.com>
2022-10-22 21:52:03 +0800

    added more sliders for noise level/light intensity

--- front-end/src/pages/jobListings/filters/JobEnvironment.js
+++ front-end/src/pages/jobListings/filters/JobEnvironment.js
@@ -4,10 +4,11 @@ const JobEnvironment = () => {
   return (
     <div className="mt-3 d-flex flex-column w-100 bg-light align-center">
       <h5 className="mx-1 mt-2 text-muted">Job Environment</h5>
-      <label htmlFor="noise" className="form-label mx-1">
+      <label htmlFor="noise" className="form-label mx-1 fw-bold">
         Noise Level
       </label>
       <div className="d-flex">
+        <p className="text-center">Min: </p>
         <p className="w-25 text-center">Quiet</p>
         <input
           type="range"
@@ -19,10 +20,37 @@ const JobEnvironment = () => {
         ></input>
         <p className="w-25 text-center">Loud</p>
       </div>
-      <label htmlFor="light" className="form-label mx-1">
+      <div className="d-flex">
+        <p className="text-center">Max: </p>
+        <p className="w-25 text-center">Quiet</p>
+        <input
+          type="range"
+          className="form-range w-50"
+          min="0"
+          max="4"
+          step="1"
+          id="noise"
+        ></input>
+        <p className="w-25 text-center">Loud</p>
+      </div>
+      <label htmlFor="light" className="form-label mx-1 fw-bold">
         Light Intensity
       </label>
       <div className="d-flex">
+        <p className="text-center">Min: </p>
+        <p className="w-25 text-center">Dim</p>
+        <input
+          type="range"
+          className="form-range w-50"
+          min="0"
+          max="4"
+          step="1"
+          id="light"
+        ></input>
+        <p className="w-25 text-center">Bright, Flashing</p>
+      </div>
+      <div className="d-flex">
+        <p className="text-center">Max: </p>
         <p className="w-25 text-center">Dim</p>
         <input
           type="range"
