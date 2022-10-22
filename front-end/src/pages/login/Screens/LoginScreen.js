f7ec75a9a8ffa704a453ad71412634a6fcd8dc4d
onthewaysia <atsqw001@gmail.com>
2022-10-22 17:36:00 +0800

    Update LoginScreen.js

--- front-end/src/pages/login/Screens/LoginScreen.js
+++ front-end/src/pages/login/Screens/LoginScreen.js
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LoginScreen = () => {
+  const [user, setUser] = useState({});
+
+  const handleUsername = (e) => {
+    setUser({ ...user, username: e.target.value });
+  };
+  const handlePassword = (e) => {
+    setUser({ ...user, password: e.target.value });
+  };
+
+  const handleLogin = (e) => {
+    if (user.username && user.password) {
+      return postLogin();
+    } else {
+      alert("Missing username/password input");
+    }
+  };
+  async function postLogin(
+    url = "http://127.0.0.1:5001/api/users/login",
+    data = user
+  ) {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const jResponse = await response.json();
+    if (jResponse.message) {
+      alert(`Invalid username/password: ${jResponse.message}`);
+    } else {
+      setUser({ ...jResponse });
+      console.log(user);
+      alert(jResponse);
+    }
+    return jResponse;
+  }
+
   return (
     <div>
       <div className="form-outline m-4 row">
-        <input type="text" className="form-control" placeholder="Username" />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Username"
+          onChange={handleUsername}
+        />
       </div>
 
       <div className="form-outline m-4 row">
@@ -12,11 +55,14 @@ const LoginScreen = () => {
           type="password"
           className="form-control"
           placeholder="Password"
+          onChange={handlePassword}
         />
       </div>
 
       <div className="row dflex justify-content-center m-4">
-        <button className="btn btn-warning w-25">Submit</button>
+        <button className="btn btn-warning w-25" onClick={handleLogin}>
+          Submit
+        </button>
       </div>
     </div>
   );
