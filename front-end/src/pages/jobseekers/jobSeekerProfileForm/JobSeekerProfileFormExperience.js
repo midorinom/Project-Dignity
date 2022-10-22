46cf1897e21fb3a9c1e49549aa34a316bf171a65
Steph Luo <steph.luokl@gmail.com>
2022-10-23 02:44:55 +0800

    integrated with react-hook-form, data can be captured

--- front-end/src/pages/jobseekers/jobSeekerProfileForm/JobSeekerProfileFormExperience.js
+++ front-end/src/pages/jobseekers/jobSeekerProfileForm/JobSeekerProfileFormExperience.js
@@ -1,42 +1,217 @@
 import React, { useState } from "react";
+import { useForm, useFieldArray } from "react-hook-form";
 import styles from "./jobSeekerProfileForm.module.css";
 import JobSeekerNewExperience from "./JobSeekerNewExperience";
 
 const JobSeekerProfileFormExperience = (props) => {
-  const [addNewExperience, setAddNewExperience] = useState([
-    <JobSeekerNewExperience key={0} />,
-  ]);
+  // const [addNewExperience, setAddNewExperience] = useState([
+  //   <JobSeekerNewExperience key={0} />,
+  // ]);
 
-  const handleAddExperience = (e) => {
-    e.preventDefault();
-    setAddNewExperience([
-      ...addNewExperience,
-      <JobSeekerNewExperience key={addNewExperience.length} />,
-    ]);
-  };
+  // const handleAddExperience = (e) => {
+  //   e.preventDefault();
+  //   setAddNewExperience([
+  //     ...addNewExperience,
+  //     <JobSeekerNewExperience key={addNewExperience.length} />,
+  //   ]);
+  // };
 
   function goToEducation() {
     props.setCurrentPage("Education");
   }
+
+  // adding react-hook-forms functionality
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      experienceSet: [
+        {
+          title: "",
+          type: "",
+          company: "",
+          startDate: "",
+          endDate: "",
+          jobDesc: "",
+        },
+      ],
+    },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "experienceSet", // an array of objects
+  });
+
+  const onSubmit = (data) => {
+    props.setExperienceSchema(data);
+    console.log("data: ", data);
+    console.log(data.experienceSet[0].title);
+  };
+
+  const onError = (errors) => console.log(errors);
 
   return (
     <section
       className="container-md"
       id="jobSeekerProfileForm-ExperienceSection"
     >
-      <form id="jobSeekerProfileForm-Experience">
+      <form
+        onSubmit={handleSubmit(onSubmit, onError)}
+        id="jobSeekerProfileForm-Experience"
+      >
         <div className="row m-5">
           <div className="col-md-8">
-            {addNewExperience}
-            {/*<-------------------- add new experience -------------------->*/}
+            {/* -------------------- add new experience object -------------------- */}
+            {/* {addNewExperience} */}
+            {fields.map((item, index) => {
+              return (
+                <div key={item.id}>
+                  {/*<-------------------------- job title -------------------------->*/}
+                  <div className="form-group mb-4">
+                    <label className="form-label" htmlFor="job-title">
+                      Job Title
+                    </label>
+                    <input
+                      className="form-control p-3"
+                      id="job-title"
+                      name={`experienceSet.${index}.title`}
+                      type="text"
+                      placeholder={"e.g Cataloguing Assistant"}
+                      {...register(`experienceSet.${index}.title`, {
+                        required: true,
+                      })}
+                    ></input>
+                    {/* {errors.experienceSet[index].title && (
+                      <p className="mt-2 text-danger">
+                        {errors.experienceSet[index].title.message}
+                      </p>
+                    )} */}
+                  </div>
+                  {/*<-------------------------- job type -------------------------->*/}
+                  <div className="form-group mb-4">
+                    <label className="form-label" htmlFor="job-type">
+                      Job Type
+                    </label>
+                    <select
+                      className="form-select p-3"
+                      id="job-type"
+                      name={`experienceSet.${index}.type`}
+                      {...register(`experienceSet.${index}.type`, {
+                        required: true,
+                      })}
+                    >
+                      <option value="">Select from drop down list</option>
+                      <option value="Baker">Baker</option>
+                      <option value="Barista">Barista</option>
+                      <option value="Cashier">Cashier</option>
+                      <option value="Packer">Packer</option>
+                      <option value="Waiter">Waiter</option>
+                    </select>
+                  </div>
+                  {/*<------------------------ company name ------------------------>*/}
+                  <div className="form-group mb-4">
+                    <label className="form-label" htmlFor="company-name">
+                      Company Name
+                    </label>
+                    <input
+                      className="form-control p-3"
+                      id="company-name"
+                      name={`experienceSet.${index}.company`}
+                      type="text"
+                      placeholder="e.g. Starbucks"
+                      {...register(`experienceSet.${index}.company`, {
+                        required: true,
+                      })}
+                    ></input>
+                  </div>
+                  {/*<------------------------- start date ------------------------->*/}
+                  <div className="form-group mb-4">
+                    <label className="form-label" htmlFor="start-date-job">
+                      Start Date
+                    </label>
+                    <input
+                      className="form-control p-3"
+                      id="start-date-job"
+                      name={`experienceSet.${index}.startDate`}
+                      type="date"
+                      {...register(`experienceSet.${index}.startDate`, {
+                        required: true,
+                      })}
+                    ></input>
+                  </div>
+                  {/*<-------------------------- end date -------------------------->*/}
+                  <div className="form-group mb-4">
+                    <label className="form-label" htmlFor="end-date-job">
+                      End Date
+                    </label>
+                    <input
+                      className="form-control mb-2 p-3"
+                      id="end-date-job"
+                      name={`experienceSet.${index}.endDate`}
+                      type="date"
+                      {...register(`experienceSet.${index}.endDate`)}
+                    ></input>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        id="flexCheckDefault-experience"
+                      ></input>
+                      <label
+                        className="form-check-label text-muted"
+                        htmlFor="flexCheckDefault-experience"
+                      >
+                        I am currently working here
+                      </label>
+                    </div>
+                  </div>
+                  {/*<---------------------- job description ---------------------->*/}
+                  <div className="form-group mb-4">
+                    <label className="form-label" htmlFor="job-description">
+                      Description
+                    </label>
+                    <textarea
+                      className="form-control mb-2 p-3"
+                      type="text"
+                      id="job-description"
+                      name={`experienceSet.${index}.jobDesc`}
+                      style={{ height: 200 }}
+                      placeholder={"Enter here"}
+                      {...register(`experienceSet.${index}.jobDesc`, {
+                        required: true,
+                      })}
+                    ></textarea>
+                    <small className="text-muted">
+                      200 / 200 characters left
+                    </small>
+                  </div>
+                  {/*<-------------------- delete skill button -------------------->*/}
+                  <div className="form-group align-content-end mb-4">
+                    <button
+                      type="button"
+                      className={`${styles.circle_btn} btn btn-outline-dark btn-sm bi-dash-lg mt-3`}
+                      onClick={() => remove(index)}
+                    ></button>
+                    <label className="form-label" htmlFor="add-new-skill">
+                      Delete skill
+                    </label>
+                  </div>
+                  <hr></hr>
+                </div>
+              );
+            })}
+            {/*<-------------------- add new experience button -------------------->*/}
             <div className="form-group mb-4">
               <button
-                className={styles.circle_btn}
-                onClick={handleAddExperience}
+                className={`${styles.circle_btn} btn btn-outline-dark btn-sm bi-plus-lg mt-3`}
+                // onClick={handleAddExperience}
+                onClick={() => append()}
                 id="add-new-skill"
-              >
-                <i className="bi bi-plus-lg"></i>
-              </button>
+              ></button>
               <label className="form-label" htmlFor="add-new-skill">
                 Add Another Experience
               </label>
@@ -47,7 +222,10 @@ const JobSeekerProfileFormExperience = (props) => {
           {/* <------------------------ side panel ------------------------> */}
           <div className="col-md-3">
             <div className=" sidePanel row mt-5">
-              <button className={`${styles.side_buttons} mt-3 mb-4 p-3`}>
+              <button
+                type="submit"
+                className={`${styles.side_buttons} mt-3 mb-4 p-3`}
+              >
                 Save Changes
               </button>
               <button className={`${styles.side_buttons} mb-4 p-3`}>
