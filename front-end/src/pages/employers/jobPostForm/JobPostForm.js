807aaeab9c6339d1ce2ac946571ee5a14b0bd30d
Eileenpngg <eileenpng1843@gmail.com>
2022-10-22 23:21:06 +0800

    'banner routing done css'

--- front-end/src/pages/employers/jobPostForm/JobPostForm.js
+++ front-end/src/pages/employers/jobPostForm/JobPostForm.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import JobAbout from "./JobAbout";
 import EmployerAccessibility from "./EmployerAccessibility";
+
 const JobPost = () => {
-  return <div className="centered"><EmployerAccessibility/></div>;
+  const [currentPage, setCurrentPage] = useState("About");
+  // Render the current page
+  function manageCurrentPage(e) {
+    setCurrentPage(e.target.innerText);
+  }
+
+  // Render the landing page depending on what type of user is logged in
+  function displayCurrentPage() {
+    switch (currentPage) {
+      case "About The Job":
+        return <JobAbout setCurrentPage={setCurrentPage} />;
+      case "Accessibilty Considerations":
+        return <EmployerAccessibility setCurrentPage={setCurrentPage} />;
+    }
+  }
+  const page = displayCurrentPage();
+
+  return (
+    <>
+      <div className="centered">Banner with Profile Photo</div>
+      <div className="centered">
+        <div onClick={manageCurrentPage}>About The Job</div>
+        <div onClick={manageCurrentPage}>Accessibilty Considerations</div>
+      </div>
+      {page}
+    </>
+  );
 };
 
 export default JobPost;
