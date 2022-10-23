72951033a4f505730be629d00bde22fe83c1cef0
Steph Luo <steph.luokl@gmail.com>
2022-10-23 19:39:36 +0800

    header banner wip

--- front-end/src/pages/jobseekers/jobSeekerProfileForm/JobSeekerProfileForm.js
+++ front-end/src/pages/jobseekers/jobSeekerProfileForm/JobSeekerProfileForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styles from "./jobSeekerProfileFormHeader.module.css";
 import JobSeekerProfileFormAbout from "./JobSeekerProfileFormAbout";
 import JobSeekerProfileFormSkills from "./JobSeekerProfileFormSkills";
 import JobSeekerProfileFormAbilityDiff from "./JobSeekerProfileFormAbilityDiff";
@@ -8,12 +9,12 @@ import EmployerProfileForm from "../../employers/employerProfileForm/EmployerPro
 
 const JobSeekerProfileForm = () => {
   const [currentPage, setCurrentPage] = useState("About");
-  // const [profile, setProfile] = useState()
   const [aboutSchema, setAboutSchema] = useState();
   const [skillsSchema, setSkillsSchema] = useState();
   const [abilityDifferencesSchema, setAbilityDifferencesSchema] = useState();
   const [experienceSchema, setExperienceSchema] = useState();
   const [educationSchema, setEducationSchema] = useState();
+  // const [profile, setProfile] = useState()
 
   // Render the current page
   function manageCurrentPage(e) {
@@ -69,15 +70,70 @@ const JobSeekerProfileForm = () => {
 
   return (
     <>
-      <div className="centered">Banner with Profile Photo</div>
-      <div className="centered">
-        <div onClick={manageCurrentPage}>About</div>
-        <div onClick={manageCurrentPage}>Skills</div>
-        <div onClick={manageCurrentPage}>Ability Differences</div>
-        <div onClick={manageCurrentPage}>Experience</div>
-        <div onClick={manageCurrentPage}>Education</div>
-      </div>
-      {page}
+      <header>
+        {/* Banner */}
+        <div className="container-md">
+          <div className="row" id={styles.banner}>
+            <div className="d-flex justify-content-md-center m-0 p-0">
+              <div className="col-md-2 m-3">
+                <embed />
+              </div>
+              <div className="col-md-8 p-4">
+                <h1 className=" mt-4 mb-3">{`This is my name`}</h1>
+                <p style={{ fontSize: "1.3em" }}>{`This is my aspiration`}</p>
+                <p>{`This is my personal brand statement`}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Form section buttons */}
+        <div
+          className="row w-100 m-0"
+          style={{
+            height: 58,
+            boxShadow: "1px 2px 3px 4px rgba(20, 20, 20, 0.2)",
+          }}
+        >
+          <div className="d-flex justify-content-md-center">
+            <div
+              className={`${styles.sectionButtons} p-3`}
+              type="button"
+              onClick={manageCurrentPage}
+            >
+              About
+            </div>
+            <div
+              className={`${styles.sectionButtons} p-3`}
+              type="button"
+              onClick={manageCurrentPage}
+            >
+              Skills
+            </div>
+            <div
+              className={`${styles.sectionButtons} p-3`}
+              type="button"
+              onClick={manageCurrentPage}
+            >
+              Ability Differences
+            </div>
+            <div
+              className={`${styles.sectionButtons} p-3`}
+              type="button"
+              onClick={manageCurrentPage}
+            >
+              Experience
+            </div>
+            <div
+              className={`${styles.sectionButtons} p-3`}
+              type="button"
+              onClick={manageCurrentPage}
+            >
+              Education
+            </div>
+          </div>
+        </div>
+      </header>
+      <main className="mt-5">{page}</main>
     </>
   );
 };
