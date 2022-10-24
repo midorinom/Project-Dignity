9e7b1040db26c4bfc5d68f4298d6691845d06d1c
midorinom <107839253+midorinom@users.noreply.github.com>
2022-10-25 01:02:01 +0800

    updated JobSeekerProfile to extract data correctly

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -22,7 +22,7 @@ function App() {
   // =========
   // Change this userType initial value to jobSeeker/employer if you need to access those landing/profile/profileForm pages
 
-  const [userDetails, setUserDetails] = useState({ type: "" });
+  const [userDetails, setUserDetails] = useState({ type: "jobSeeker" });
   const [searchInput, setSearchInput] = useState("");
   const [isSearch, setIsSearch] = useState(false);
   const [selectedJobPost, setSelectedJobPost] = useState({});
