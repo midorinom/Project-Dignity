6088d672070437788023c1d27f47684e27919867
onthewaysia <atsqw001@gmail.com>
2022-10-25 12:22:41 +0800

    Landing pages done

--- front-end/src/pages/jobseekers/jobSeekerLanding/JobSeekerLanding.js
+++ front-end/src/pages/jobseekers/jobSeekerLanding/JobSeekerLanding.js
@@ -9,7 +9,7 @@ import jsLandingIcon3 from "./taskIcons/jsLanding3.png";
 const JobSeekerLanding = (props) => {
   const userCtx = useContext(UserContext);
   return (
-    <div>
+    <div className="p-5">
       <Search
         setSearchInput={props.setSearchInput}
         setIsSearch={props.setIsSearch}
@@ -18,12 +18,29 @@ const JobSeekerLanding = (props) => {
       <div className="m-4">
         {userCtx.userDetails.type === "jobSeeker" && (
           <div>
-            <h1 className="display-3">Manage My Job Search</h1>
+            <h1 className="display-5">Manage My Job Search</h1>
+            <div className="row">
+              <div className="col-4 d-flex justify-content-center flex-column align-item-center">
+                <p className="text-center d-flex align-items-center justify-content-center my-0 p-4 w-100 mr-1 bg-dark text-white">
+                  My Applied Jobs
+                </p>
+              </div>
+              <div className="col-4 d-flex justify-content-center flex-column align-item-center">
+                <p className="text-center d-flex align-items-center justify-content-center m-0 p-4 w-100 bg-dark text-white">
+                  My Saved Jobs
+                </p>
+              </div>
+              <div className="col-4 d-flex justify-content-center flex-column align-item-center">
+                <p className="text-center d-flex align-items-center justify-content-center m-0 p-4 w-100 ml-1 bg-dark text-white">
+                  My CV
+                </p>
+              </div>
+            </div>
           </div>
         )}
 
-        <div>
-          <h1 className="display-3">What would you like to do today</h1>
+        <div className="my-5">
+          <h1 className="display-5">What would you like to do today</h1>
           <div className="row">
             <div className="col-4 d-flex justify-content-center flex-column align-item-center">
               <img
@@ -45,6 +62,45 @@ const JobSeekerLanding = (props) => {
               <img src={jsLandingIcon3} alt="task icon to job list" />
               <p className="text-center d-flex align-items-center justify-content-center m-0 p-4 w-100 ml-1 bg-warning">
                 I want to know more about inclusive employers
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="my-5">
+          <h1 className="display-5">Popular Topics</h1>
+          <div className="row mb-4">
+            <div className="col-4 d-flex justify-content-center flex-column align-item-center">
+              <p className="text-center d-flex align-items-center justify-content-center my-0 p-4 w-100 mr-1 border border-secondary">
+                Job Shadowing
+              </p>
+            </div>
+            <div className="col-4 d-flex justify-content-center flex-column align-item-center">
+              <p className="text-center d-flex align-items-center justify-content-center m-0 p-4 w-100 border border-secondary">
+                Assistive Technology Fund
+              </p>
+            </div>
+            <div className="col-4 d-flex justify-content-center flex-column align-item-center">
+              <p className="text-center d-flex align-items-center justify-content-center m-0 p-4 w-100 ml-1 border border-secondary">
+                Find an Employment Coach
+              </p>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-4 d-flex justify-content-center flex-column align-item-center">
+              <p className="text-center d-flex align-items-center justify-content-center my-0 p-4 w-100 mr-1 border border-secondary">
+                Internships
+              </p>
+            </div>
+            <div className="col-4 d-flex justify-content-center flex-column align-item-center">
+              <p className="text-center d-flex align-items-center justify-content-center m-0 p-4 w-100 border border-secondary">
+                Stories of Differently-abled Persons at Work
+              </p>
+            </div>
+            <div className="col-4 d-flex justify-content-center flex-column align-item-center">
+              <p className="text-center d-flex align-items-center justify-content-center m-0 p-4 w-100 ml-1 border border-secondary">
+                Training Programmes
               </p>
             </div>
           </div>
