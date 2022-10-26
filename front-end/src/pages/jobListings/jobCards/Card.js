483b040e85dd03379e38cbb8cf03e08c7c6600bf
haofang <haofang97@gmail.com>
2022-10-26 21:15:16 +0800

    prevent non-jobSeekers from seeing save button

--- front-end/src/pages/jobListings/jobCards/Card.js
+++ front-end/src/pages/jobListings/jobCards/Card.js
@@ -122,7 +122,7 @@ const Card = (props) => {
     <div className="container mb-3 mx-0 px-0">
       <div className="row w-100 bg-light mx-0" onClick={handleJobDetails}>
         <div className="col-1 d-flex flex-column align-items-center mt-3">
-          {!click && (
+          {!click && userCtx.userDetails.type === "jobSeeker" && (
             <>
               <img
                 src={save}
@@ -135,7 +135,7 @@ const Card = (props) => {
               </p>
             </>
           )}
-          {click && (
+          {click && userCtx.userDetails.type === "jobSeeker" && (
             <>
               <div
                 style={{ transform: "scale(2)" }}
